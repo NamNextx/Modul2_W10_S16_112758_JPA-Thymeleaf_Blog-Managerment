eb3b686db2d8089cc0c21df489691fa499ea23fa
Namnextx <phuongnam.nextx@gmail.com>
2019-04-30 07:49:09 +0700

    add update form

--- src/main/java/com/codegym/cms/controller/BlogController.java
+++ src/main/java/com/codegym/cms/controller/BlogController.java
@@ -1,11 +1,15 @@
 package com.codegym.cms.controller;
 
 import com.codegym.cms.model.Blog;
+import com.codegym.cms.repository.IBlogRepository;
 import com.codegym.cms.service.IBlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -21,11 +25,33 @@ public class BlogController {
         modelAndView.addObject("blogList",blogList);
         return modelAndView;
     }
-    @GetMapping("/create-blog")
 
+    @GetMapping("/create-blog")
     public ModelAndView createBlogTemplate(){
         ModelAndView modelAndView=new ModelAndView("/repository/create_blog");
         modelAndView.addObject("blog",new Blog());
         return modelAndView;
     }
+
+    @PostMapping("/create-blog")
+    public String createBlog(Blog blog, RedirectAttributes redirect){
+        iBlogService.save(blog);
+        redirect.addFlashAttribute("done","created new a blog");
+        return ("redirect:/home");
+    }
+
+    @GetMapping("/update_blog/{id}")
+    public ModelAndView updateBlog(@ModelAttribute("id") Long id){
+        Blog blog= iBlogService.findById(id);
+        ModelAndView modelAndView;
+        if (blog==null){
+            modelAndView=new ModelAndView("/repository/error-404");
+            return modelAndView;
+        }else {
+            modelAndView=new ModelAndView("/repository/update_form");
+            modelAndView.addObject("blog",blog);
+            return modelAndView;
+        }
+    }
+
 }
